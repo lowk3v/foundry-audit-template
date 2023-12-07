fee8a138ab11239c8735246e8b5c92546f93b2c5
LowK <davidkevinit@gmail.com>
2023-12-07 14:19:19 +0700

    fix sign message

--- test/utils/Signature.sol
+++ test/utils/Signature.sol
@@ -8,6 +8,6 @@ contract SignatureHelper is Test {
     function sign65chars(uint256 pk, bytes32 data) public view returns (bytes memory signature) {
         bytes32 toEthSignedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", data));
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(pk, toEthSignedMessageHash);
-        signature = abi.encodePacked(v, r, s);
+        signature = abi.encodePacked(r, s, v);
     }
 }
